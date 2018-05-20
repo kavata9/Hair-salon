9c4c0add2025337259d0be30031c1cd75b1bdfe5
agnes musyoka <kavatamusyoka24@gmail.com>
2018-05-20 17:19:49 +0300

    check if stylist name breider initiates correctly

---
+++ src/main/java/Stylist.java
@@ -0,0 +1,36 @@
+import java.util.List;
+import org.sql2o.*;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Stylist {
+    private String name;
+    private int id;
+
+    public Stylist(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static List<Stylist> all() {
+        String sql = "SELECT id, name FROM stylist";
+        try(Connection con = DB.sql2o.open()) {
+          return con.createQuery(sql).executeAndFetch(Stylist.class);
+        }
+      }
+
+    public int getId() {
+        return id;
+    }
+
+    // public List<Client> getClient() {
+    // }
+
+    public static Stylist find(int id) {
+        return find(id - 1);
+    }
+
+}
---
+++ src/test/java/StylistTest.java
@@ -0,0 +1,27 @@
+import org.sql2o.*;
+    import org.junit.*;
+    import static org.junit.Assert.*;
+
+    public class StylistTest {
+
+      @Before
+      public void setUp() {
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test", "agnes", "a9%Mrdvs");
+      }
+
+      @After
+      public void tearDown() {
+        try(Connection con = DB.sql2o.open()) {
+          String deleteClientQuery = "DELETE FROM client *;";
+          String deleteStylistQuery = "DELETE FROM stylist *;";
+          con.createQuery(deleteClientQuery).executeUpdate();
+          con.createQuery(deleteStylistQuery).executeUpdate();
+        }
+      }
+      @Test
+    public void getName_StylistInstantiatesWithName_Braider() {
+        Stylist testStylist = new Stylist("Breider");
+        assertEquals("Breider", testStylist.getName());
+    }
+}
+        
