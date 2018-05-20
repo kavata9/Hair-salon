8a9e3280abeb4b85bd18ed6b1f843f45919dca78
agnes musyoka <kavatamusyoka24@gmail.com>
2018-05-20 18:45:17 +0300

    introduce a function to create relationship among the two database

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -3,18 +3,22 @@ import org.sql2o.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import org.sql2o.*;
 
 public class Client {
   private String description;
   private boolean completed;
   private LocalDateTime createdAt;
   private int id;
+  private int stylistId;
 
-  public Client(String description) {
+  public Client(String description, int stylistId) {
     this.description = description;
     completed = false;
     createdAt = LocalDateTime.now();
+    this.stylistId = stylistId;
   }
+
   public String getDescription() {
     return description;
   }
@@ -30,21 +34,23 @@ public class Client {
   public int getId() {
     return id;
   }
+  public int getStylistId() {
+    return stylistId;
+  }
 
   public static Client find(int id) {
-    try(Connection con = DB.sql2o.open()) {
+    try (Connection con = DB.sql2o.open()) {
       String sql = "SELECT * FROM client where id=:id";
-      Client client = con.createQuery(sql)
-        .addParameter("id", id)
-        .executeAndFetchFirst(Client.class);
+      Client client = con.createQuery(sql).addParameter("id", id).executeAndFetchFirst(Client.class);
       return client;
     }
   }
-//database initiation
+
+  // database initiation
   public static List<Client> all() {
-    String sql = "SELECT id, description FROM client";
+    String sql = "SELECT id, description, stylistId FROM client";
     try(Connection con = DB.sql2o.open()) {
-      return con.createQuery(sql).executeAndFetch(Client.class);
+     return con.createQuery(sql).executeAndFetch(Client.class);
     }
   }
 
@@ -55,18 +61,21 @@ public class Client {
     } else {
       Client newClient = (Client) otherClient;
       return this.getDescription().equals(newClient.getDescription()) &&
-             this.getId() == newClient.getId();
+             this.getId() == newClient.getId() &&
+             this.getStylistId() == newClient.getStylistId();
     }
   }
 
-public void save() {
-  try(Connection con = DB.sql2o.open()) {
-    String sql = "INSERT INTO client(description) VALUES (:description)";
-    this.id = (int) con.createQuery(sql, true)
-      .addParameter("description", this.description)
-      .executeUpdate()
-      .getKey();
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO client(description, stylistId) VALUES (:description, :categoryId)";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("description", this.description)
+        .addParameter("categoryId", this.stylistId)
+        .executeUpdate()
+        .getKey();
+    }
   }
-}
+
 
 }
--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -20,29 +20,29 @@ public class ClientTest {
   @Test
 
   public void Client_instantiatesCorrectly_true() {
-    Client myClient = new Client("Victor");
+    Client myClient = new Client("Agnes", 1);
     assertEquals(true, myClient instanceof Client);
   }
 
   @Test
   public void equals_returnsTrueIfDescriptionsAretheSame() {
-    Client firstClient = new Client("Agnes");
-    Client secondClient = new Client("Agnes");
+    Client firstClient = new Client("Agnes", 1);
+    Client secondClient = new Client("Agnes", 1);
     assertTrue(firstClient.equals(secondClient));
   }
 
   @Test
   public void save_returnsTrueIfDescriptionsAretheSame() {
-    Client myClient = new Client("Agnes");
+    Client myClient = new Client("Agnes",1);
     myClient.save();
     assertTrue(Client.all().get(0).equals(myClient));
   }
 
   @Test
   public void all_returnsAllInstancesOfClient_true() {
-    Client firstClient = new Client("Agnes");
+    Client firstClient = new Client("Agnes", 1);
     firstClient.save();
-    Client secondClient = new Client("Mary");
+    Client secondClient = new Client("Mary", 1);
     secondClient.save();
     assertEquals(true, Client.all().get(0).equals(firstClient));
     assertEquals(true, Client.all().get(1).equals(secondClient));
@@ -50,7 +50,7 @@ public class ClientTest {
 
   @Test
   public void save_assignsIdToObject() {
-    Client myClient = new Client("Agnes");
+    Client myClient = new Client("Agnes", 1);
     myClient.save();
     Client savedClient = Client.all().get(0);
     assertEquals(myClient.getId(), savedClient.getId());
@@ -58,16 +58,26 @@ public class ClientTest {
 
   @Test
   public void getId_clientInstantiateWithAnID() {
-    Client myClient = new Client("Agnes");
+    Client myClient = new Client("Agnes", 1);
     myClient.save();
     assertTrue(myClient.getId() > 0);
   }
   @Test
   public void find_returnsClientWithSameId_secondClient() {
-    Client firstClient = new Client(" Agnes");
+    Client firstClient = new Client("Agnes", 1);
     firstClient.save();
-    Client secondClient = new Client("Mary");
+    Client secondClient = new Client("Mary", 1);
     secondClient.save();
     assertEquals(Client.find(secondClient.getId()), secondClient);
   }
+
+  @Test
+      public void save_savesStylistIdIntoDB_true() {
+        Stylist myStylist = new Stylist("Breider");
+        myStylist.save();
+        Client myClient = new Client("Agnes", myStylist.getId());
+        myClient.save();
+        Client savedClient = Client.find(myClient.getId());
+        assertEquals(savedClient.getStylistId(), myStylist.getId());
+      }
 }
